4fb0c369611f05a5de3773237dcf79f42ca472fa
Max Marttinen <max.marttinen@amanngirrbach.com>
2023-03-06 16:15:33 +0100

    Coding Challenge Nodejs

---
+++ pyramid/index.js
@@ -0,0 +1,21 @@
+/**
+
+Print a pyramid!
+
+    *
+   ***
+  *****
+ *******
+*********
+
+ACCEPTANCE CRITERIA:
+
+Write a script to output pyramid of given size to the console (with leading spaces).
+
+*/
+
+function pyramid(size = 5) {
+  console.log("A beautiful pyramid")
+}
+
+pyramid(5)
